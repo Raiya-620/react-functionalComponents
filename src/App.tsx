cac8dd41f3a46365a2c8af2e2ca548ccc6ad4329
Suraiya <medsusu713@gmail.com>
2025-05-12 14:25:12 +0000

    completion of ui assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import BlogCard from "./components/Card";
+import img from "./assets/Rectangle.png";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex gap-2 justify-center items-center min-h-screen">
+        <BlogCard
+          title={"The future of technology is Healthcare"}
+          summary={
+            "Exploring how empowering technologies with AI and IoT are transforming patient care and medical research"
+          }
+          coverImage={img}
+          tags={["Healthcare", "Healthcare", "Healthcare", "+1"]}
+        />
+        <BlogCard
+          title={"The future of technology is Healthcare"}
+          summary={
+            "Exploring how empowering technologies with AI and IoT are transforming patient care and medical research"
+          }
+          coverImage={img}
+          tags={["Healthcare", "Healthcare", "Healthcare", "+1"]}
+        />
+        <BlogCard
+          title={"The future of technology is Healthcare"}
+          summary={
+            "Exploring how empowering technologies with AI and IoT are transforming patient care and medical research"
+          }
+          coverImage={img}
+          tags={["Healthcare", "Healthcare", "Healthcare", "+1"]}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,46 @@
-function BlogCard() {
-  return <></>;
+import { FaUser } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
+interface Props {
+  title: string;
+  summary: string;
+  coverImage: string;
+  tags: string[];
+}
+
+function BlogCard({ title, summary, coverImage, tags }: Props) {
+  return (
+    <>
+      <div className="w-[420px] border-2 border-gray-200 flex rounded flex-col shadow">
+        <div className="flex flex-col  relative">
+          <img src={coverImage} alt="" className=" h-60 rounded-t" />
+          <div className="bg-white w-max rounded px-2 m-5 text-gray-600 absolute">
+            Tech
+          </div>
+        </div>
+        <div className=" p-5 flex flex-col gap-4 ">
+          <h1 className="font-bold text-2xl">{title}</h1>
+          <p className="text-gray-600 ">{summary}</p>
+          <div className="flex flex-row gap-2">
+            {tags.map((tag) => (
+              <div className="border-1 border-gray-400 rounded-2xl w-max px-2 py-1">
+                {tag}
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-between text-gray-400">
+            <div className="flex items-center">
+              <FaUser />
+              <p className="ml-2">Dr. Sarah John</p>
+            </div>
+            <div className="flex items-center ">
+              <FaCalendarAlt />
+              <p className="ml-2 mr-5">Mar 15, 2024</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BlogCard;
